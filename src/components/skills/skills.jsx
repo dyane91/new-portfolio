8ce506450eff1324c990da0aa4443b9f6547822d
Dyane Avalos <dyane_r@yahoo.es>
2020-04-18 02:15:34 -0400

    footer done

--- src/components/skills/skills.jsx
+++ src/components/skills/skills.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Row, Col, Card, Badge }  from 'react-bootstrap';
+import { Row, Col, Badge }  from 'react-bootstrap';
 
 export class Skills extends Component {
   render() {
